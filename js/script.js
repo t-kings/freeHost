a5b4c772628675153a7661ed0509692e65e16d3c
tkings <nwachukwu.kingsley01@yahoo.com>
2020-06-19 07:28:11 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+var acc = document.getElementsByClassName("accordion");
+var icon = document.getElementsByClassName("accordionNull");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.firstElementChild.firstElementChild.classList.toggle("accordionNullA");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var backgrounds = document.querySelectorAll(".headerCarousel");
+  var headers = document.querySelectorAll(".headerCarousel h1");
+  var headersSpan = document.querySelectorAll(".headerCarousel h1 span");
+  var paragraphs = document.querySelectorAll(".headerCarousel p");
+  var links = document.querySelectorAll(".headerCarousel a");
+  for (i = 0; i < backgrounds.length; i++) {
+    backgrounds[i].style.display = "none";
+    headers[i].style.display = "none";
+    headersSpan[i].style.display = "none";
+    paragraphs[i].style.display = "none";
+    links[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > backgrounds.length) {slideIndex = 1}
+
+  backgrounds[slideIndex-1].style.display = "block";
+  headers[slideIndex-1].style.display = "block";
+  headersSpan[slideIndex-1].style.display = "block";
+  paragraphs[slideIndex-1].style.display = "block";
+  links[slideIndex-1].style.display = "inline-block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+
+var TeamSlideIndex = 1;
+showTeamSlides(TeamSlideIndex);
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showTeamSlides(TeamSlideIndex = n);
+}
+
+function showTeamSlides(n) {
+  var i;
+  var clientImage = document.querySelectorAll(".clientImage img");
+  var clientName = document.querySelectorAll(".clientName h3");
+  var clientTitle = document.querySelectorAll(".clientTitle h6");
+  var clientMessage = document.querySelectorAll(".clientMessage p");
+  var dots = document.getElementsByClassName("dot");
+  if (n > clientImage.length) {TeamSlideIndex = 1}
+  if (n < 1) {TeamSlideIndex = clientImage.length}
+  for (i = 0; i < clientImage.length; i++) {
+    clientImage[i].style.display = "none";
+    clientName[i].style.display = "none";
+    clientTitle[i].style.display = "none";
+    clientMessage[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" dotsActive", "");
+  }
+  clientImage[TeamSlideIndex-1].style.display = "block";
+  clientName[TeamSlideIndex-1].style.display = "block";
+  clientTitle[TeamSlideIndex-1].style.display = "block";
+  clientMessage[TeamSlideIndex-1].style.display = "block";
+  dots[TeamSlideIndex-1].className += " dotsActive";
+}
+
+const navbar = document.querySelector("#toggler");
+function toggleNavigation(x) {
+  x.classList.toggle("change");
+  if(navbar.style.display ==="block"){
+    navbar.style.display = "none"
+  }else{
+     navbar.style.display = "block";
+  }
+}
